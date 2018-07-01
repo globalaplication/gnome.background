e20caa54638461c036f0c42ade6e23830dbe0262
Nuri Özdaş <32783197+globalaplication@users.noreply.github.com>
2018-07-01 10:06:42 +0000

    Update main.py

--- changed/main.py
+++ changed/main.py
@@ -1,7 +1,10 @@
 
 import os
 
-def change():
+def change(wallpaper, options):
+    center = "gsettings set org.gnome.desktop.background picture-options 'centered'"
     gnome = "gsettings set org.gnome.desktop.background picture-uri"
     wallpaper = ""
     string = "{} {}".format(gnome, wallpaper)
+    os.system(string)
+    
